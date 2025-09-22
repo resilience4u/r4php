2a54c876e9c88534b58492fdd2e7c5843bd19366
Diego Ananias <mamuthdev@gmail.com>
2025-09-22 13:49:26 -0300

    fix(types): add callable/array/request typehints to anonymous classes in Chain and Guzzle middleware

--- src/Integrations/Guzzle/R4Middleware.php
+++ src/Integrations/Guzzle/R4Middleware.php
@@ -9,15 +9,22 @@ final class R4Middleware
 {
     public static function wrap(Policy $policy): callable
     {
-        return function (callable $handler) use ($policy) {
-            return function (RequestInterface $request, array $options) use ($handler, $policy) {
-                return $policy->execute(new class($handler, $request, $options) implements Executable {
-                    public function __construct(private $handler, private $request, private $options) {}
-                    public function __invoke(): mixed {
-                        return ($this->handler)($this->request, $this->options);
-                    }
-                });
-            };
+        return function (RequestInterface $request, array $options) use ($handler, $policy) {
+            return $policy->execute(new class($handler, $request, $options) implements Executable {
+                /** @var callable */
+                private $handler;
+                private RequestInterface $request;
+                /** @var array<string,mixed> */
+                private array $options;
+                public function __construct(callable $handler, RequestInterface $request, array $options) {
+                    $this->handler = $handler;
+                    $this->request = $request;
+                    $this->options = $options;
+                }
+                public function __invoke(): mixed {
+                    return ($this->handler)($this->request, $this->options);
+                }
+            });
         };
     }
 }
