8e158393580c031129cbadbe88b0195b5230a827
Diego Ananias <mamuthdev@gmail.com>
2025-09-22 13:38:04 -0300

    docs(examples): update basic config (rate limiter limit, CB timeWindowSec)

--- examples/basic.php
+++ examples/basic.php
@@ -7,11 +7,17 @@ use Resiliente\R4PHP\Core\Factory;
 require __DIR__ . '/../vendor/autoload.php';
 
 $cfg = Config::fromArray([
-    'rateLimiter' => ['tokensPerSecond' => 20, 'burst' => 40],
+    'rateLimiter' => ['tokensPerSecond' => 20, 'limit' => 40],
     'bulkhead'    => ['maxConcurrent' => 32],
     'timeLimiter' => ['timeoutMs' => 1500],
     'retry'       => ['maxAttempts' => 5, 'initialMs' => 200, 'maxMs' => 3000, 'multiplier' => 2.0, 'jitter' => true],
-    'circuitBreaker' => ['name' => 'core', 'failureRatePct' => 50, 'minSamples' => 10, 'openMs' => 30000],
+    'circuitBreaker' => [
+        'name' => 'core',
+        'failureRatePct' => 50,
+        'minSamples' => 10,
+        'openMs' => 30000,
+        'timeWindowSec' => 10, // <-- novo
+    ],
 ]);
 
 $policy = Factory::fromConfig($cfg);
