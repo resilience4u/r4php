de931f0b7cae951d7193f5d1f436f1f78e42f973
Diego Ananias <mamuthdev@gmail.com>
2025-09-22 13:38:04 -0300

    docs(examples): point Guzzle example to local httpbin and apply resilient middleware

--- examples/guzzle.php
+++ examples/guzzle.php
@@ -9,14 +9,24 @@ use Resiliente\R4PHP\Integrations\Guzzle\R4Middleware;
 require __DIR__ . '/../vendor/autoload.php';
 
 $policy = Factory::fromConfig(Config::fromArray([
-    'retry'          => ['maxAttempts' => 4, 'initialMs' => 100, 'maxMs' => 2000, 'jitter' => true],
-    'circuitBreaker' => ['name'=>'http','failureRatePct'=>50,'minSamples'=>10,'openMs'=>15000],
+    'retry' => ['maxAttempts' => 3, 'initialMs' => 100, 'maxMs' => 500, 'multiplier' => 2.0, 'jitter' => false],
+    'circuitBreaker' => [
+        'name' => 'http',
+        'failureRatePct' => 50,
+        'minSamples' => 4,
+        'openMs' => 5000,
+        'timeWindowSec' => 5,
+    ],
 ]));
 
 $stack = HandlerStack::create();
 $stack->push(R4Middleware::wrap($policy));
 
-$client = new Client(['handler' => $stack, 'timeout' => 5.0]);
+$client = new Client([
+    'base_uri' => 'http://httpbin:8080',
+    'handler'  => $stack,
+    'timeout'  => 3.0,
+]);
 
-$res = $client->get('https://httpbin.org/status/200');
+$res = $client->get('/status/200');
 echo $res->getStatusCode(), PHP_EOL;
