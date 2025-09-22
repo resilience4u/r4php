3fc2957a63f19af2a2cb4c0d5ec77c79044fd901
Diego Ananias <mamuthdev@gmail.com>
2025-09-22 14:13:18 -0300

    chore: fixed phpstan issues

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,1 +1,12 @@
 <?php
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'no_unused_imports' => true,
+        'single_quote' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in([__DIR__.'/src', __DIR__.'/tests'])
+    );
--- src/Integrations/Guzzle/R4Middleware.php
+++ src/Integrations/Guzzle/R4Middleware.php
@@ -9,28 +9,25 @@ final class R4Middleware
 {
     public static function wrap(Policy $policy): callable
     {
-        return function (callable $handler) use ($policy): callable {
-            return function (RequestInterface $request, array $options) use ($handler, $policy) {
-                return $policy->execute(new class($handler, $request, $options) implements Executable {
-                    /** @var callable */
-                    private $handler;
-                    private RequestInterface $request;
-                    /** @var array<string,mixed> */
-                    private array $options;
+        return function (RequestInterface $request, array $options) use ($handler, $policy) {
+            return $policy->execute(new class($handler, $request, $options) implements Executable {
+                /** @var callable */
+                private $handler;
+                private RequestInterface $request;
+                /** @var array<string,mixed> */
+                private array $options;
 
-                    public function __construct(callable $handler, RequestInterface $request, array $options)
-                    {
-                        $this->handler = $handler;
-                        $this->request = $request;
-                        $this->options = $options;
-                    }
-
-                    public function __invoke(): mixed
-                    {
-                        return ($this->handler)($this->request, $this->options);
-                    }
-                });
-            };
+                /** @param array<string,mixed> $options */
+                public function __construct(callable $handler, RequestInterface $request, array $options)
+                {
+                    $this->handler = $handler;
+                    $this->request = $request;
+                    $this->options = $options;
+                }
+                public function __invoke(): mixed {
+                    return ($this->handler)($this->request, $this->options);
+                }
+            });
         };
     }
 }
