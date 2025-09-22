342a63997c30b856bea4e147b78f038258dfc7a3
Diego Ananias <mamuthdev@gmail.com>
2025-09-22 13:38:04 -0300

    test(bulkhead): block reentrant execution when bulkhead is full

--- tests/BulkheadPolicyTest.php
+++ tests/BulkheadPolicyTest.php
@@ -1,6 +1,26 @@
 <?php
+declare(strict_types=1);
 
-class BulkheadPolicyTest
+use PHPUnit\Framework\TestCase;
+use Resiliente\R4PHP\Policies\BulkheadPolicy;
+use Resiliente\R4PHP\Contracts\Executable;
+
+final class BulkheadPolicyTest extends TestCase
 {
+    public function testBlocksReentrantExecutionWhenFull(): void
+    {
+        $p = BulkheadPolicy::fromArray(['maxConcurrent' => 1]);
 
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Bulkhead full');
+
+        $p->execute(new class($p) implements Executable {
+            public function __construct(private $p) {}
+            public function __invoke(): mixed {
+                return $this->p->execute(new class implements Executable {
+                    public function __invoke(): mixed { return 'inner'; }
+                });
+            }
+        });
+    }
 }
